afdb1f6762cd63bb41ec6dcbcf93d60323e55ab3
amcp <amcp@mit.edu>
2012-12-27 00:18:20 -0500

    GPL v3!

--- worth/tax/TaxDictionary.h
+++ worth/tax/TaxDictionary.h
@@ -1,8 +1,23 @@
 /*
  * TaxDictionary.h
  *
- *  Created on: 22 √ÎÙ 2012
- *   Copyright 2012 Alexander Patrikalakis
+ * Created on: 22 √ÎÙ 2012
+ * Copyright 2012 Alexander Patrikalakis
+ *
+ * This file is part of the Worth project.
+ *
+ * Worth is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Worth is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Worth. If not, see http://www.gnu.org/licenses/.
  */
 
 #ifndef WORTH_TAX_TAXDICTIONARY_H_
